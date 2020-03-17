4b234c5ee89705bec002b060944cef7b20adeefb
AthanasiusOfAlex <lmelahn@legionaries.org>
2020-03-17 16:54:14 +0100

    Updated to SPM 5.1 format.
    Removed outdated dependency to CBSD, which does not apply to me.

--- Package.swift
+++ Package.swift
@@ -1,14 +1,16 @@
 // swift-tools-version:5.1
 import PackageDescription
 
-let deps: [Package.Dependency]
-#if os(Linux)
-	deps = [.Package(url: "https://github.com/mdaxter/CBSD.git", majorVersion: 1)]
-#else
-	deps = []
-#endif
-
 let package = Package(
-    name: "BigInteger",
-    dependencies: deps
+    name: "BigInt",
+    dependencies: [],
+	
+    targets: [
+        .target(
+            name: "BigInteger",
+            dependencies: []),
+        //.testTarget(
+        //    name: "BigIntegerTest",
+        //    dependencies: ["BigInteger"]),
+    ]
 )
